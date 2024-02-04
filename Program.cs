82bbf20e80fd951749b90366d96af2fbef65d2b7
Linda F <linda.forslund92@outlook.com>
2024-02-04 14:26:14 +0100

    Changed map name.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
-﻿using The_C__player_s_guide.Assignments.Manticore;
+﻿using The_C__player_s_guide.Assignments.BossBattles.Manticore;
 using The_C__player_s_guide.Assignments.Other.CataCombsClass.ThePoint;
 
-
-//var manticore = new Manticore();
-//manticore.Run();
+var manticore = new Manticore();
+manticore.Run();
 
 //new ThreeKings().RunThreeKings();
 //new DefenceConsolas().RunDefenceConsolas();
 //new SimulaSoup().RunSimulaSoup();
 //new Arrow().RunArrow();
-new Point().RunPoint();
+//new Point().RunPoint();
 
